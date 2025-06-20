bd9609faba68f62368195e4e6714dbe8c067245a
Roger Howell <roger.howell@education.gov.uk>
2025-06-20 09:14:54 +0100

    delete a bunch of tests to demo what happens

--- DfE.GIAP.All/DfE.GIAP.Core.UnitTests/NewsArticles/UseCases/NewsArticleWriteOnlyRepositoryTestDoubles.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DfE.GIAP.Core.UnitTests.NewsArticles.UseCases;
-internal static class NewsArticleWriteOnlyRepositoryTestDoubles
-{
-    internal static Mock<INewsArticleWriteRepository> Default() => CreateMock();
-
-    private static Mock<INewsArticleWriteRepository> CreateMock() => new();
-}
